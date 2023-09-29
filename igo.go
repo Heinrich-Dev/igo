a2b5a1b28efa815881e5a4c78b68c4896a13d976
boekhoh <boekhoh@wwu.edu>
2023-09-29 15:28:57 -0700

    Figured out how to get user input

--- igo.go
+++ igo.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	var response string
+	var responded bool = false
+	for !responded {
+		fmt.Println("Would you like to host or connect? (h/c)")
+		fmt.Scanf("%v", &response)
+		response = strings.TrimRight(response, "\n")
+		if response == "h" {
+			responded = true
+			createServer()
+		} else if response == "c" {
+			responded = true
+			join()
+		} else {
+			fmt.Println("Did not specify connection!")
+		}
+	}
+}
+
+func createServer() {
+	fmt.Println("created server")
+}
+
+func join() {
+	fmt.Println("joined server")
 }
