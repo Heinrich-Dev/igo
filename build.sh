0398c255476174849308de31e14c82725e21226b
boekhoh <boekhoh@wwu.edu>
2023-10-02 23:35:57 -0700

    nuked the tmux idea since it caused too many issues, will try new build script later

--- build.sh
+++ build.sh
@@ -1,18 +1,2 @@
 #!/bin/bash
-# Assumes script is run in host pane in tmux
 go build
-
-buildResult=$(echo $?)
-if [$buildResult != 0]; then
-    exit 1
-fi
-# tmux isnt running, create a session and add a new window
-tmuxResult=$(pgrep tmux)
-if ["$tmuxResult" == ""]; then
-    tmux new -s igo 'go run .' \; split-window -h './igo'
-else
-    tmux select-pane -t 0
-    ./igo
-    tmux select-pane -t 1
-    ./igo
-fi
