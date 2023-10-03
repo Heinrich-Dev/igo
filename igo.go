fad55ef364f937a757d32a438010c7f3c2aeed0c
boekhoh <boekhoh@wwu.edu>
2023-10-02 23:57:16 -0700

    added new returns to createServer and join functions

--- igo.go
+++ igo.go
@@ -6,9 +6,8 @@ Henry Boekhoff
 igo.go - Creating the Go board game in Go. Two clients ssh'd on the same
 machine can play with one another.
 REFERENCES:
-Loosely Based on Lachlan Imel's termChess: https://github.com/brochacho01/termChess
+Inspired by Lachlan Imel's termChess: https://github.com/brochacho01/termChess
 Creating and accepting TCP connections: https://pkg.go.dev/net
-
 */
 
 import (
@@ -24,59 +23,56 @@ const (
 
 func main() {
 	var response string
-	//var connectionType int // 0 for host, 1 for client
+	var connectionType int // 0 for host, 1 for client
 	var responded bool = false
+	var connection net.Conn // interface for client or server, returned by helper functions
 	for !responded {
 		fmt.Println("Would you like to host or connect? (h/c)")
 		fmt.Scanln(&response)
 		if response == "h" {
 			responded = true
-			//connectionType = 0
-			createServer()
+			connectionType, connection = createServer()
 		} else if response == "c" {
 			responded = true
-			//connectionType = 1
-			join()
+			connectionType, connection = join()
 		} else {
 			fmt.Println("Did not specify connection type!")
 		}
-
 	}
-
+	if connectionType == -1 {
+		fmt.Fprintf(os.Stderr, "Error while trying to establish connection between client and server\n")
+	}
+	if connectionType == 0 {
+		connection.Write([]byte("TESTING"))
+	} else {
+		message := make([]byte, 1024)
+		connection.Read(message)
+		fmt.Printf("%s\n", message)
+	}
 }
 
-func createServer() int {
+func createServer() (int, net.Conn) {
 	fmt.Println("Starting server on port 8080...")
 	listener, err := net.Listen("tcp", HOST+":"+PORT)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Listen error\n")
-		return 1
+		return -1, nil
 	}
 	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Accept error\n")
-		return 1
+		return -1, nil
 	}
-	numBytes, err := conn.Write([]byte("TESTING"))
-	if numBytes == 0 {
-		fmt.Fprintf(os.Stderr, "Read, somehow, read 0 bytes\n")
-	}
-	return 0
+
+	return 0, conn
 }
 
-func join() int {
+func join() (int, net.Conn) {
 	fmt.Println("Joining local server...")
 	conn, err := net.Dial("tcp", HOST+":"+PORT)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Dial error\n")
-		return 1
+		return -1, nil
 	}
-
-	message := make([]byte, 1024)
-	numBytes, err := conn.Read(message)
-	if err != nil || numBytes == 0 {
-		fmt.Fprintf(os.Stderr, "Read failed\n")
-	}
-	fmt.Printf("%s\n", message)
-	return 0
+	return 1, conn
 }
