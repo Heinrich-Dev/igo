c59b6cfcd04224c248694b13d81ca191a992887d
Heinrich-Dev <boekhoffhenry@gmail.com>
2023-10-09 21:21:42 -0700

    changed build script

--- build.sh
+++ build.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-go build
+go build igo.go board.go
 
 # change this variable to 1 if tmux isn't your thing, and
 # you would rather just build the application
 tmuxPref=0
 
-if [[ $tmuxPref == 1 ]]; then
-    exit 0
-fi
 buildResult=$(echo $?)
 
 if [[ $buildResult -ne 0 ]]; then
     exit 1
 fi
 
-tmuxOpen=$(pgrep tmux)
-
-if [[ "$tmuxOpen" == "" ]]; then
-    tmux new -s igo \; split-window -h \; select-pane -t 0 \; send-keys './igo'
+if [[ $tmuxPref == 1 ]]; then
+    exit 0
 fi
+
+tmux split-window -h
+tmux select-pane -t 1
+tmux send-keys './igo'
+tmux select-pane -t 0
+tmux send-keys './igo'
